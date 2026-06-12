f044060afe0402c80576a2882a97d0254ffec535
dimonchikvl <dimonchikvl122@gmail.com>
2026-06-12 17:02:45 +0300

    Update game.py

--- game.py
+++ game.py
@@ -2,10 +2,24 @@ from db import get_user, update_user
 
 
 def xp_needed(level):
-    return 100 + (level - 1) * 50
+    return 100 + (level - 1) * 60
 
 
-def add_xp(uid: int, amount: int, stat: str = None):
+COOLDOWN = {}
+
+
+def can_click(uid):
+    import time
+    now = time.time()
+
+    if uid in COOLDOWN and now - COOLDOWN[uid] < 3:
+        return False
+
+    COOLDOWN[uid] = now
+    return True
+
+
+def add_xp(uid, amount, stat=None):
     user = get_user(uid)
 
     xp = user[1]
@@ -15,12 +29,9 @@ def add_xp(uid: int, amount: int, stat: str = None):
     discipline = user[4]
     finance = user[5]
     content = user[6]
-    streak = user[7]
 
-    # ADD XP
     xp += amount
 
-    # ADD STAT
     if stat == "strength":
         strength += 1
     elif stat == "discipline":
@@ -30,7 +41,6 @@ def add_xp(uid: int, amount: int, stat: str = None):
     elif stat == "content":
         content += 1
 
-    # LEVEL UP SYSTEM
     leveled_up = False
 
     while xp >= xp_needed(level):
@@ -38,14 +48,12 @@ def add_xp(uid: int, amount: int, stat: str = None):
         level += 1
         leveled_up = True
 
-    # SAVE TO DB
     update_user(uid, "xp", xp)
     update_user(uid, "level", level)
     update_user(uid, "strength", strength)
     update_user(uid, "discipline", discipline)
     update_user(uid, "finance", finance)
     update_user(uid, "content", content)
-    update_user(uid, "streak", streak)
 
     return {
         "xp": xp,
@@ -58,7 +66,6 @@ def add_xp(uid: int, amount: int, stat: str = None):
     }
 
 
-# TASK REWARDS
 TASKS = {
     "train": (40, "strength"),
     "steps": (30, "discipline"),
