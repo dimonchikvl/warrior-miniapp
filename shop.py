01391615c654ab9c16e959edf34ab852c1a4b228
dimonchikvl <dimonchikvl122@gmail.com>
2026-06-12 17:08:36 +0300

    Add shop dictionary with item boosts and costs

---
+++ shop.py
@@ -0,0 +1,14 @@
+SHOP = {
+    "energy_boost": {
+        "cost": 100,
+        "effect": "energy+20"
+    },
+    "xp_boost": {
+        "cost": 150,
+        "effect": "xp_multiplier"
+    },
+    "coin_boost": {
+        "cost": 120,
+        "effect": "coins+50"
+    }
+}
