ccdeb4163e37b01791130c8c503682e0c1c93212
dimonchikvl <dimonchikvl122@gmail.com>
2026-06-12 16:29:45 +0300

    Update template directory path to use BASE_DIR

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 @app.get("/")
 async def home(request: Request):
