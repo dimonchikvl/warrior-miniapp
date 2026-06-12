3734fa19b35dbd0508e785a206110c799f968ec9
dimonchikvl <dimonchikvl122@gmail.com>
2026-06-12 16:59:26 +0300

    Refactor add_xp function with type hints and new features
    Refactor add_xp function to include type hints and improve comments. Added streak tracking and updated user data accordingly.

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ def xp_needed(level):
     return 100 + (level - 1) * 50
 
 
-def add_xp(uid, amount, stat=None):
+def add_xp(uid: int, amount: int, stat: str = None):
     user = get_user(uid)
 
     xp = user[1]
@@ -15,10 +15,12 @@ def add_xp(uid, amount, stat=None):
     discipline = user[4]
     finance = user[5]
     content = user[6]
+    streak = user[7]
 
-    # XP + stat
+    # ADD XP
     xp += amount
 
+    # ADD STAT
     if stat == "strength":
         strength += 1
     elif stat == "discipline":
@@ -28,15 +30,39 @@ def add_xp(uid, amount, stat=None):
     elif stat == "content":
         content += 1
 
-    # LEVEL UP
+    # LEVEL UP SYSTEM
+    leveled_up = False
+
     while xp >= xp_needed(level):
         xp -= xp_needed(level)
         level += 1
+        leveled_up = True
 
-    # SAVE
+    # SAVE TO DB
     update_user(uid, "xp", xp)
     update_user(uid, "level", level)
     update_user(uid, "strength", strength)
     update_user(uid, "discipline", discipline)
     update_user(uid, "finance", finance)
     update_user(uid, "content", content)
+    update_user(uid, "streak", streak)
+
+    return {
+        "xp": xp,
+        "level": level,
+        "strength": strength,
+        "discipline": discipline,
+        "finance": finance,
+        "content": content,
+        "leveled_up": leveled_up
+    }
+
+
+# TASK REWARDS
+TASKS = {
+    "train": (40, "strength"),
+    "steps": (30, "discipline"),
+    "no_smoke": (50, "discipline"),
+    "video": (40, "content"),
+    "book": (20, "discipline")
+}
